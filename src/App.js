c54ff44df483293cf77b483133eb3deb729efb69
Jude <jeremiahjude07@gmail.com>
2022-09-07 17:20:33 +0530

    carousel adding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { data } from "./mockData";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function App() {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="relative flex items-center">
+        <MdChevronLeft
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+        />
+        <div
+          id="slider"
+          className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {data.map((item) => (
+            <img
+              className="w-[220px] h-[220px]  inline-block p-2  cursor-pointer hover:scale-105 ease-in-out duration-300"
+              src={item.img}
+              alt="/"
+            />
+          ))}
+        </div>
+        <MdChevronRight
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/mockData.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1662514282655-438b87b04995?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1662401208927-0b71a8635a84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1662529739712-930d83814dac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1662514462162-60747334936a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1662530067420-0c57c0680b96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1662293938028-04bf486cddae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1662529744948-e398f9e43880?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 8,
+    img: "https://images.unsplash.com/photo-1662377088227-74d5f42f8280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  },
+];
